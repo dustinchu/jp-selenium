a71e89625e6a318814b6b61f505ced7c7511c380
wisplu <dustin_chu@hotmail.com>
2019-01-10 23:12:04 +0800

    edit list to json

--- getUrbanProData.py
+++ getUrbanProData.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # export PATH=$PATH:/home/muru/myFlaskPython
-from pyvirtualdisplay import Display
 from selenium import webdriver
 import time
+import lxml
+import json
 from bs4 import BeautifulSoup
 import unicodedata
+from matplotlib.cbook import flatten
 
 # def get():
     # opt=webdriver.ChromeOptions()
@@ -50,6 +52,17 @@ def get():
     titleStr=""
     #內容字串分割儲存
     bodyStr=""
+
+
+
+
+
+    title = []
+    url = []
+    time = []
+    img = []
+    newsJson = []
+
     #遍歷HTML 先將 class top-news-list找出來
     for post in soup.find_all('section', "top-news-list"):
         # print("post==========",post)
@@ -66,15 +79,17 @@ def get():
                 '''
                 if titleImg.img:
                     print("標題圖片==", titleImg.img['src'])
+                    img.append(titleImg.img['src'])
+
 
             #標題內容
-            for title in titleItem.find_all('em', "title"):
-                print("標題內容==", title)
+            for name in titleItem.find_all('em', "title"):
+                print("標題內容==", name)
             # print("內容標題==", bodyTtile)
             # 剛開始遍歷先將字串清空
             titleStr = ""
             # 將字串分割
-            for string in title.strings:
+            for string in name.strings:
                 # 判斷是不是漢字
                 if is_japanese(string):
                     # 是漢字的話漢字後面加上&
@@ -92,15 +107,21 @@ def get():
             for titleTime in titleItem.find_all('time', "time"):
                 #.string 這樣可以只找出裡面的字串
                 if titleTime.string:
+                    time.append(titleTime.string)
                     print("標題時間==", titleTime.string)
 
             #得到<a裡面href 標題頁面的URL
             for titleUrl in titleItem.find_all('a', href=True):
                 if titleUrl.text:
+                    url.append(titleUrl['href'])
                     print("標題url==", titleUrl['href'])
-        print(titleStr)
+
+        title.append(titleStr)
+
+        # json_string = json.dumps([ob.__dict__ for ob in titlejson])
         #內容
         for body in post.find_all("ul", "top-news-list__items news-list-grid"):
+
             # print("body==========", body)
             #得到內容 時間 4個 item
             for bodyItem in body.find_all('h1', "news-list-item__title"):
@@ -128,28 +149,47 @@ def get():
                     #如果只要<ruby> 可以使用這樣
                     # if bodyTtile.ruby:
 
+                title.append(bodyStr)
                 #時間
                 for bodyTime in bodyItem.find_all('time', "time"):
                     if bodyTime.string:
+                        time.append(bodyTime.string)
                         print("內容時間==", bodyTime.string)
                 #URL
                 for bodyUrl in bodyItem.find_all('a', href=True):
                     if bodyUrl.text:
+                        url.append(bodyUrl['href'])
                         print("內容URL==", bodyUrl['href'])
 
             for bodyImg in body.find_all('figure', "news-list-item__image"):
-                print(bodyImg)
+                # print(bodyImg)
                 if bodyImg.img:
+                    img.append(bodyImg.img['src'])
                     print("內容圖片==", bodyImg.img['src'])
 
-            print(bodyStr)
+
+            # print(newsJson)
+            # newsJson += list(flatten(zip(title, img, time, url)))
+
+    for a, b, c, d in zip(title, img, time, url):
+        result = {}
+        result['title'] = a
+        result['img'] = b
+        result['time'] = c
+        result['url'] = d
+        newsJson.append(result)
+    newsJson = json.dumps(newsJson)
+
+    for i in newsJson:
+        print(i)
+
     driver.close()
     return "ok"
 
 def is_japanese(string):
     for ch in string:
         name = unicodedata.name(ch)
-        if "CJK UNIFIED" in name :
+        if "CJK UNIFIED" in name:
             return True
         return False
 
