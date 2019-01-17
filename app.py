ede033004be280840e93ec8cc8ccffe5ff843f2c
dustin_chu <dustin_chu@hotmail.com>
2019-01-17 20:25:03 +0800

    Merge branch 'master' of https://github.com/dustinchu/jp-selenium
    # Conflicts:
    #	getUrbanProData.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
 import getUrbanProData as gd
+import os
 
-app = Flask(__name__)
+from flask_apscheduler import APScheduler
+from apscheduler.schedulers.blocking import BlockingScheduler
+
+
+
+
+app = Flask(__name__)  # 實例化flask
+
 
 @app.route("/")
 def hello():
@@ -9,6 +17,9 @@ def hello():
     return gd.get()
 
 
+
+
 if __name__ == '__main__':
+
     app.run(debug=True, host="0.0.0.0",port=5000)
     #app.run("--host=127.0.0.1 --port=1234")
